228a41eb8d28c86d1aea322a6a4df0b760bacef2
zhangshiyun321@sina.com <zhangshiyun321@sina.com>
2018-05-18 18:02:54 +0800

    【需求】springboot框架搭建 【说明】整合添加log4j2日志打印

---
+++ xiaopang-common/src/main/java/com/xiaopang/common/Test.java
@@ -0,0 +1,14 @@
+package com.xiaopang.common;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Test {
+    private static Logger logger = LogManager.getLogger(Test.class.getName());
+    public static void main (String[] args) {
+        logger.debug("创建的活动id："+111);
+        logger.info("创建的活动id："+222);
+        logger.warn("创建的活动id："+333);
+        logger.error("创建的活动id："+444);
+    }
+}
